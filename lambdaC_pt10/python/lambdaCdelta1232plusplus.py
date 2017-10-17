2221eee311dfc37f840f3d8a8503cc93a70d3949
Rui <ruixiao491@yahoo.com>
2017-10-17 00:10:39 -0400

    fourth commit 	new file:   lambdaC_pt10/python/lambdaCdelta1232plusplus.py 	new file:   lambdaC_pt10/python/lambdaCdelta1232plusplus.pyc 	new file:   lambdaC_pt10/python/lambdaClambda1520.py 	new file:   lambdaC_pt10/python/lambdaClambda1520.pyc 	new file:   lambdaC_pt10/python/lambdaCpkpi.py 	new file:   lambdaC_pt10/python/lambdaCpkpi.pyc 	new file:   lambdaC_pt10/python/lambdaCpkstar892.py 	new file:   lambdaC_pt10/python/lambdaCpkstar892.pyc

---
+++ lambdaC_pt10/python/lambdaCdelta1232plusplus.py
@@ -0,0 +1,58 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.Pythia8CUEP8M1Settings_cfi import *
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(5020.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            decay_table = cms.string('Run2Ana/lambdapkpi/data/DECAY_2010.DEC'),
+            operates_on_particles = cms.vint32(),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt.pdl'),
+            user_decay_file = cms.vstring('Run2Ana/lambdapkpi/data/lambdaC_deltaplusplus.dec'),
+            list_forced_decays = cms.vstring('MylambdaC+','Myanti-lambdaC-') 
+        ),
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CUEP8M1SettingsBlock,
+        processParameters = cms.vstring(     
+            'HardQCD:all = on',
+            'PhaseSpace:pTHatMin = 4.', #min pthat
+        ),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CUEP8M1Settings',
+            'processParameters',
+        )
+    )
+)
+
+generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)
+
+lambdaCDaufilter = cms.EDFilter("PythiaMomDauFilter",
+    ParticleID = cms.untracked.int32(4122),
+    MomMinPt = cms.untracked.double(10.),
+	MomMaxPt = cms.untracked.double(500.),
+    MomMinEta = cms.untracked.double(-2.4),
+    MomMaxEta = cms.untracked.double(2.4),
+    DaughterIDs = cms.untracked.vint32(2224, -321),
+    NumberDaughters = cms.untracked.int32(2),
+    DaughterID = cms.untracked.int32(2224),
+    DescendantsIDs = cms.untracked.vint32(2212,211),
+    NumberDescendants = cms.untracked.int32(2),
+)
+
+lambdaCrapidityfilter = cms.EDFilter("PythiaFilter",
+       ParticleID = cms.untracked.int32(4122),
+                                  MinPt = cms.untracked.double(10.),
+								  MaxPt = cms.untracked.double(500.),
+								  MinRapidity = cms.untracked.double(-1.2),
+								  MaxRapidity = cms.untracked.double(1.2),
+								  )
+ProductionFilterSequence = cms.Sequence(generator*lambdaCDaufilter*lambdaCrapidityfilter)
